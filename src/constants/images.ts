1f48357b4a3c72e5332b2ee5a1d92f7d4e28841e
robbuild <roble.oma@gmail.com>
2024-11-22 19:48:18 +0300

    change skyskraper image

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,7 +1,7 @@
 export const images = {
     // Hero section images
     hero: {
-      skyscraper: "https://images.unsplash.com/photo-1545127398-14699f92334b?auto=format&fit=crop&q=80&w=2070",
+      skyscraper: "https://plus.unsplash.com/premium_photo-1671196038062-fe8c047b00ee?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&q=80&w=2070",
       apartment: "https://images.unsplash.com/photo-1669071192880-0a94316e6e09?auto=format&fit=crop&q=80&w=2070",
       villa: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?auto=format&fit=crop&q=80&w=2071"
     },
@@ -9,7 +9,7 @@ export const images = {
     // Service cards
     services: {
       commercial: "https://images.unsplash.com/photo-1545127398-14699f92334b?auto=format&fit=crop&q=80&w=2070",
-      residential: "https://images.unsplash.com/photo-1669071192880-0a94316e6e09?auto=format&fit=crop&q=80&w=2070",
+      residential: "https://plus.unsplash.com/premium_photo-1693966067602-9ca1f56695c0?q=80&w=1854&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D?auto=format&fit=crop&q=80&w=2070",
       luxury: "https://images.unsplash.com/photo-1613490493576-7fde63acd811?auto=format&fit=crop&q=80&w=2071"
     },
     
